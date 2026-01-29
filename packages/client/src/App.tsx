1fa54102cfb4285394a70b9cca462de326c9c108
DucNguyenV0 <115515416+ducnguyenNOPRO@users.noreply.github.com>
2026-01-28 17:28:54 -0800

    Initial Commit

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  return (
+    <>
+      <div className="font-bold">{message}</div>
+          <Button variant="outline">Click me</Button>
+    </>
+  )
+}
+
+export default App;
