12c438a5e6a77348779bba7120f7f8157fa7d5f6
DucNguyenV0 <115515416+ducnguyenNOPRO@users.noreply.github.com>
2026-01-30 22:02:13 -0800

    Validate Register Data

--- packages/client/src/pages/register.tsx
+++ packages/client/src/pages/register.tsx
@@ -1,53 +1,105 @@
 import { Input } from "@/components/ui/input";
+import { userSchema, type UserFormFields } from "@/lib/zodSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm, type SubmitHandler } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<UserFormFields>({
+    resolver: zodResolver(userSchema),
+  });
+
+  const onSubmit: SubmitHandler<UserFormFields> = async (data) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log(data);
+  };
+
   return (
     <div className="h-screen w-screen bg-gray-100 flex items-center justify-center">
       <div className="w-120 bg-white shadow rounded-lg flex flex-col p-5 gap-3">
         <h1 className="text-2xl text-center font-bold text-gray-900">
           Create an Account
         </h1>
-        <Input label="First Name" placeholder="John" id="firstName" required />
-        <Input label="Last Name" placeholder="Doe" id="lastName" required />
-        <Input
-          label="Email"
-          placeholder="johndoe@gmail.com"
-          id="email"
-          required
-        />
-        <Input
-          label="Password"
-          placeholder="Enter your password"
-          id="password"
-          required
-        />
-        <Input
-          label="Confirm Password"
-          placeholder="Enter your password again"
-          id="passwordConfirm"
-          required
-        />
-        <div>
-          <input type="checkbox" className="mr-2" />
-          <span className="">
-            I have read and consent to the
-            <Link to="/login" className="text-blue-500 hover:underline">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-3">
+          <Input
+            label="First Name"
+            placeholder="John"
+            id="firstName"
+            name="firstName"
+            required
+            register={register}
+            error={errors.firstName?.message}
+          />
+          <Input
+            label="Last Name"
+            placeholder="Doe"
+            id="lastName"
+            name="lastName"
+            required
+            register={register}
+            error={errors.lastName?.message}
+          />
+          <Input
+            label="Email"
+            placeholder="johndoe@gmail.com"
+            id="email"
+            name="email"
+            required
+            register={register}
+            error={errors.email?.message}
+          />
+          <Input
+            label="Password"
+            placeholder="Enter your password"
+            type="password"
+            id="password"
+            name="password"
+            required
+            register={register}
+            error={errors.password?.message}
+          />
+          <Input
+            label="Confirm Password"
+            placeholder="Enter your password again"
+            type="password"
+            id="passwordConfirm"
+            name="passwordConfirm"
+            required
+            register={register}
+            error={errors.passwordConfirm?.message}
+          />
+
+          {errors.root && (
+            <span className="text-red-500">{errors.root.message}</span>
+          )}
+          <div>
+            <input type="checkbox" className="mr-2" />
+            <span className="">
+              I have read and consent to the
+              <Link to="/login" className="text-blue-500 hover:underline">
+                {" "}
+                terms of service
+              </Link>
+            </span>
+          </div>
+          <button
+            className="bg-blue-500 text-white hover:bg-blue-700 px-3 py-2 rounded-md cursor-pointer"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "Loading..." : "Create Account"}
+          </button>
+          <div className="text-center text-sm">
+            Already have an account?
+            <Link to="/login" className="text-sm hover:underline text-blue-500">
               {" "}
-              terms of service
+              Login here.
             </Link>
-          </span>
-        </div>
-        <button className="bg-blue-500 text-white hover:bg-blue-700 px-3 py-2 rounded-md cursor-pointer">
-          Log In
-        </button>
-        <div className="text-center text-sm">
-          Already have an account?
-          <Link to="/login" className="text-sm hover:underline text-blue-500">
-            {" "}
-            Login here.
-          </Link>
-        </div>
+          </div>
+        </form>
       </div>
     </div>
   );
