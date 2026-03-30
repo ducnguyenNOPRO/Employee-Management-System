892470c97b500cbebc9da51f0f0397bb989f46e2
DucNguyenV0 <115515416+ducnguyenNOPRO@users.noreply.github.com>
2026-03-29 17:09:03 -0700

    Add a leave_balance table 1. Remove UNPAID and OTHER leave type 2. Remove CANCELLED leave status

--- packages/client/src/lib/utils.ts
+++ packages/client/src/lib/utils.ts
@@ -11,8 +11,6 @@ export function getTypeColor(type: string) {
       return "bg-blue-100 text-blue-800";
     case "SICK_LEAVE":
       return "bg-purple-100 text-purple-800";
-    case "UNPAID":
-      return "bg-orange-100 text-orange-800";
     default:
       return "bg-gray-100 text-gray-800";
   }
