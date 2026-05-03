95e3f0dccf674ace14bf831c3888a3b3e38ec2bb
DucNguyenV0 <115515416+ducnguyenNOPRO@users.noreply.github.com>
2026-05-02 23:00:51 -0700

    deploy local DB and add tracing

--- packages/tests/playwright.config.ts
+++ packages/tests/playwright.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
 
   use: {
     baseURL: "http://localhost:5173",
-    trace: "on-first-retry",
+    trace: "retain-on-failure",
+    video: "on-first-retry",
 
     // Screenshot on failure
     screenshot: "only-on-failure",
